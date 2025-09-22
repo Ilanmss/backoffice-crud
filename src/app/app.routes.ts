b9a4b7c62107b90a57e534f6e85d4cd2b6b7b36e
Ilanmss <73974608+Ilanmss@users.noreply.github.com>
2025-09-22 11:21:18 -0300

    feat: criando componente de solicitações

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MinhasTabelasComponent } from './pages/minhas-tabelas/minhas-tabelas.component';
 import { ListarTabelasComponent } from './pages/listar-tabelas/listar-tabelas.component';
+import { SolicitacoesComponent } from './pages/solicitacoes/solicitacoes.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
   {
     path: 'listar-tabelas', 
     component: ListarTabelasComponent
+  },
+  {
+    path: 'solicitacoes', 
+    component: SolicitacoesComponent
   },
 
 ];

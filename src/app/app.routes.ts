373b3d4fcfbfab03b7f58d5cf55091ee5de6869f
Ilanmss <73974608+Ilanmss@users.noreply.github.com>
2025-09-22 13:17:33 -0300

    feat: cadastrar tabelas e atualizacao de doc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MinhasTabelasComponent } from './pages/minhas-tabelas/minhas-tabelas.component';
 import { ListarTabelasComponent } from './pages/listar-tabelas/listar-tabelas.component';
 import { SolicitacoesComponent } from './pages/solicitacoes/solicitacoes.component';
+import { CadastrarTabelaComponent } from './pages/cadastrar-tabela/cadastrar-tabela.component';
 
 export const routes: Routes = [
   {
@@ -20,6 +21,10 @@ export const routes: Routes = [
   {
     path: 'solicitacoes', 
     component: SolicitacoesComponent
+  },
+  {
+    path: 'cadastrar-tabela', 
+    component: CadastrarTabelaComponent
   },
 
 ];

53036a80abb61bb17172e9222d8236d36f1874d0
Ilanmss <73974608+Ilanmss@users.noreply.github.com>
2025-09-22 14:38:37 -0300

    feat: fazendo os testes unitarios

--- src/app/pages/solicitacoes/solicitacoes.component.spec.ts
+++ src/app/pages/solicitacoes/solicitacoes.component.spec.ts
@@ -1,23 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { SolicitacoesComponent } from './solicitacoes.component';
+import { BuscarTabelasService } from '../../shared/services/buscar-tabelas/buscar-tabelas.service';
+import { of } from 'rxjs';
 
-describe('SolicitacoesComponent', () => {
+describe('Dado o componente Solicitacoes', () => {
   let component: SolicitacoesComponent;
-  let fixture: ComponentFixture<SolicitacoesComponent>;
+  let serviceSpy: jasmine.SpyObj<BuscarTabelasService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [SolicitacoesComponent]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    serviceSpy = jasmine.createSpyObj('BuscarTabelasService', ['getTabelasQueNaoPossuiAcesso']);
+    serviceSpy.getTabelasQueNaoPossuiAcesso.and.returnValue(of([{ status: 'pendente' } as any]));
 
-    fixture = TestBed.createComponent(SolicitacoesComponent);
+    TestBed.configureTestingModule({
+      providers: [{ provide: BuscarTabelasService, useValue: serviceSpy }],
+      imports: [SolicitacoesComponent],
+      declarations: []
+    });
+    const fixture = TestBed.createComponent(SolicitacoesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Então deve carregar as tabelas de solicitações ao inicializar', () => {
+    expect(component.tabelasSolicitacoes.length).toBe(1);
+    expect(component.tabelasSolicitacoes[0].status).toBe('pendente');
+    expect(component.loading).toBeFalse();
   });
 });
